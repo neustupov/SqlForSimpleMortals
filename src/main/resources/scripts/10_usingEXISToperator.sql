7af9fd705d49b50aef88d3269c933331f41bfe85
neustupov <neustupov@yandex.ru>
2018-08-06 15:45:20 +0300

    added homework examples for 12 chapter

--- src/main/resources/scripts/10_usingEXISToperator.sql
+++ src/main/resources/scripts/10_usingEXISToperator.sql
@@ -47,3 +47,44 @@ WHERE NOT exists(
 
 -- 1
 
+SELECT *
+FROM salespeople out
+WHERE exists(
+    SELECT *
+    FROM customers inn
+    WHERE out.snum = inn.snum
+          AND rating > 300
+);
+
+-- 2
+
+SELECT
+  first.snum,
+  first.sname
+FROM salespeople first, customers second
+WHERE first.snum = second.snum
+      AND second.rating > 300;
+
+-- 3
+
+SELECT *
+FROM salespeople first
+WHERE exists(
+    SELECT *
+    FROM customers second
+    WHERE first.city = second.city
+          AND first.snum <> second.snum
+);
+
+-- 4
+
+SELECT *
+FROM customers a
+WHERE exists(
+    SELECT *
+    FROM salespeople b
+    WHERE a.snum = b.snum
+          AND 1 < (SELECT count(*)
+                   FROM orders
+                   WHERE orders.snum = b.snum
+                         AND orders.cnum <> a.cnum));
