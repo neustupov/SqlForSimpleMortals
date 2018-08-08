8a299a6931f70c1918609dd3d046cb66a6032fa8
neustupov <neustupov@yandex.ru>
2018-08-08 20:59:24 +0300

    added script with examples for ANY and ALL operators

---
+++ src/main/resources/scripts/11_usingAnyAllSomeOperators.sql
@@ -0,0 +1,119 @@
+-- Using ANY operator
+
+SELECT *
+FROM salespeople
+WHERE city = ANY (
+  SELECT city
+  FROM customers
+);
+
+-- Using ANY with inequality
+
+SELECT *
+FROM salespeople
+WHERE sname < ANY (
+  SELECT cname
+  FROM customers
+);
+
+-- Using EXIST as an alternative ANY
+
+SELECT *
+FROM salespeople out
+WHERE exists(
+    SELECT *
+    FROM customers inn
+    WHERE out.sname < inn.cname
+);
+
+-- Greater than ANY in the interpretation of SQL
+
+SELECT *
+FROM customers
+WHERE rating > ANY (
+  SELECT rating
+  FROM customers
+  WHERE city = 'Rome'
+);
+
+-- Selection of records, the value amt field which exceeds ANY on October 6
+
+SELECT *
+FROM orders
+WHERE amt > ANY (
+  SELECT amt
+  FROM orders
+  WHERE odate = '06/10/1990'
+);
+
+-- Using ANY with JOIN
+
+SELECT *
+FROM orders
+WHERE amt < ANY (
+  SELECT amt
+  FROM orders a, customers b
+  WHERE a.cnum = b.cnum
+        AND b.city = 'San Jose'
+);
+
+-- Using assembly function instead ANY
+
+SELECT *
+FROM orders
+WHERE amt <
+      (SELECT max(amt)
+       FROM orders a, customers b
+       WHERE a.cnum = b.cnum
+             AND b.city = 'San Jose');
+
+-- Using ALL operator
+
+SELECT *
+FROM customers
+WHERE rating > ALL
+      (SELECT rating
+       FROM customers
+       WHERE city = 'Rome');
+
+-- Using EXIST as alternative of ALL
+
+SELECT *
+FROM customers out
+WHERE NOT exists(
+    SELECT *
+    FROM customers inn
+    WHERE out.rating <= inn.rating
+          AND inn.city = 'Rome'
+);
+
+-- Using ALL with <>
+
+SELECT *
+FROM customers
+WHERE rating <> ALL
+      (SELECT rating
+       FROM customers
+       WHERE city = 'San Jose');
+
+-- Using EXIST with connected subqueries
+
+SELECT *
+FROM customers out
+WHERE NOT exists(
+    SELECT *
+    FROM customers inn
+    WHERE out.rating <= inn.rating
+          AND inn.city = 'Rome'
+);
+
+-- Using COUNT instead EXIST
+
+SELECT *
+FROM customers out
+WHERE 1 > (
+    SELECT count(*)
+    FROM customers inn
+    WHERE out.rating <= inn.rating
+          AND inn.city = 'Rome'
+);
